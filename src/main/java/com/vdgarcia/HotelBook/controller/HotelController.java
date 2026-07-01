2e1e471abe4877b6578c0bef201fdef701951c7e
victord <victordaniel.123rt@gmail.com>
2026-07-01 10:47:48 -0600

    Documentando

--- src/main/java/com/vdgarcia/HotelBook/controller/HotelController.java
+++ src/main/java/com/vdgarcia/HotelBook/controller/HotelController.java
@@ -3,6 +3,10 @@ package com.vdgarcia.HotelBook.controller;
 import com.vdgarcia.HotelBook.dto.ClienteDTO;
 import com.vdgarcia.HotelBook.dto.HotelDTO;
 import com.vdgarcia.HotelBook.service.HotelService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,29 +16,55 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/hotel")
 @RequiredArgsConstructor
+@Tag(name = "Hoteles", description = "Controlador para la gestión y creación de hoteles")
 public class HotelController {
     private final HotelService service;
 
     @GetMapping
+    @Operation(summary = "Listar todas los hoteles", description = "Muestra todos los registros hasta el momento de los hoteles que se han realizado")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista desplegada con exito"),
+            @ApiResponse(responseCode = "400", description = "Datos de entrada inválidos o inconsistentes")
+    })
     public ResponseEntity<List<HotelDTO>> buscarTodas(){
         return ResponseEntity.ok(service.buscarTodos());
     }
     @GetMapping("/{id}")
+    @Operation(summary = "Encontrar un hotel", description = "Busca un hotel en especifica por medio de un identificador único")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Elemento encontrado y desplegado con exito"),
+            @ApiResponse(responseCode = "400", description = "Datos de entrada inválidos o inconsistentes")
+    })
     public ResponseEntity<HotelDTO> buscarPorId(@PathVariable("id") Long id){
         return ResponseEntity.ok(service.buscarPorId(id));
     }
 
     @PostMapping
+    @Operation(summary = "Crear un nuevo hotel", description = "Registra un hotel en el sistema. No se debe enviar el ID en el cuerpo.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Hotel creado con éxito"),
+            @ApiResponse(responseCode = "400", description = "Datos de entrada inválidos o inconsistentes")
+    })
     public ResponseEntity<HotelDTO> crear(@RequestBody HotelDTO dto){
         return ResponseEntity.ok(service.crear(dto));
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Actualiza un hotel", description = "Hace la actualización correspondiente de un hotel. Es necesario incluir el id en el cuerpo del URI")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Actualización exitosa"),
+            @ApiResponse(responseCode = "400", description = "Datos de entrada inválidos o inconsistentes")
+    })
     public ResponseEntity<HotelDTO> actualizar(@PathVariable Long id, @RequestBody HotelDTO dto){
         return ResponseEntity.ok(service.actualizar(id,dto));
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Elimina un hotel", description = "Elimina un hotel creado con anterioridad. Es necesario el ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200/2004", description = "Elemento eliminado con exito"),
+            @ApiResponse(responseCode = "400", description = "Datos de entrada inválidos o inconsistentes")
+    })
     public ResponseEntity<HotelDTO> eliminar (@PathVariable Long id){
         return ResponseEntity.ok(service.eliminar(id));
     }
--- src/main/java/com/vdgarcia/HotelBook/dto/HotelDTO.java
+++ src/main/java/com/vdgarcia/HotelBook/dto/HotelDTO.java
@@ -1,6 +1,8 @@
 package com.vdgarcia.HotelBook.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
+import org.yaml.snakeyaml.events.ScalarEvent;
 
 import java.util.List;
 
@@ -9,11 +11,17 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Schema(description = "Modelo que representa la información de un hotel")
 public class HotelDTO {
+    @Schema(description = "ID único del hotel (autogenerado)", example = "3", accessMode = Schema.AccessMode.READ_ONLY)
     private Long id;
+    @Schema(description = "Nombre con el que ubica el hotel", example = "Los cedros", requiredMode = Schema.RequiredMode.REQUIRED)
     private String nombre;
+    @Schema(description = "Ciudad en la que se encuentra ubicado el hotel", example = "México", requiredMode = Schema.RequiredMode.REQUIRED)
     private String ciudad;
+    @Schema(description = "Dirección principal del hotel", example = "Calle Las Heras 550", requiredMode = Schema.RequiredMode.REQUIRED)
     private String direccion;
+    @Schema(description = "Nivel de categoria del hotel", example = "3 ESTRELLAS", requiredMode = Schema.RequiredMode.REQUIRED)
     private String categoria;
     private List<HabitacionDTO> habitaciones;
 }
