f66da7de93306bbd27239a2cc2eda3ce07d1e142
victord <victordaniel.123rt@gmail.com>
2026-07-02 12:36:35 -0600

    Pruebas de Integracion con JUNIT 5

--- src/test/java/com/vdgarcia/HotelBook/integration/HabitacionServiceTest.java
+++ src/test/java/com/vdgarcia/HotelBook/integration/HabitacionServiceTest.java
@@ -1,4 +1,39 @@
 package com.vdgarcia.HotelBook.integration;
 
+
+import com.vdgarcia.HotelBook.dto.HabitacionDTO;
+import com.vdgarcia.HotelBook.service.HabitacionService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+@SpringBootTest
+@ActiveProfiles("test")
 public class HabitacionServiceTest {
+    @Autowired
+    private HabitacionService service;
+
+
+    @Test
+    @Transactional
+    void testBuscarTodos(){
+        List<HabitacionDTO> habitaciones = this.service.buscarTodos();
+        assertEquals(1,habitaciones.size());
+    }
+
+
+    @Test
+    @Transactional
+    void testBuscarPorId(){
+        Long id = 3L;
+        HabitacionDTO habitacion = this.service.buscarPorId(id);
+        assertNotNull(habitacion);
+        assertEquals(105, habitacion.getNumero());
+        assertEquals(3L,habitacion.getId());
+    }
+
 }
